84517df52a987ba77e8bc1e11d4bd47edabe7ef1
Mateusz Kowalczyk <mateusz.kowalczyk.0305@gmail.com>
2024-03-14 11:24:02 +0100

    koniec zad1

--- lab_3/zad1/main.c
+++ lab_3/zad1/main.c
@@ -4,6 +4,11 @@
 int main()
 {
 	// wywołanie funkcji zeros:
+	printf("Dla a: 3.13, b: 10.21, c: 1.45.\n");
 	zeros(3.13, 10.21, 1.45);
+	printf("Dla a: -7, b: 1, c: -13.\n");
+	zeros(-7, 1, -13);
+	printf("Dla a: 1, b: -4, c: 4.\n");
+	zeros(1, -4, 4);
 	return 0;
 }
