778bb466862d36c47a1c7bcca3e5a0e670370eed
Mateusz Kowalczyk <mateusz.kowalczyk.0305@gmail.com>
2024-03-14 01:23:04 +0100

    zad1

---
+++ lab_3/zad1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "zeros.h"
+
+int main()
+{
+	// wywołanie funkcji zeros:
+	zeros(3.13, 10.21, 1.45);
+	return 0;
+}
--- lab_3/zad1/zeros.c
+++ lab_3/zad1/zeros.c
@@ -2,20 +2,24 @@
 #include <stdio.h>
 #include <math.h>
 
-void zeros (float z, float b, float c)
+void zeros (float a, float b, float c)
 {
-	if (a > 0)
-	{
-		float delta = ((b*b)- (4*a*c));
-	}
+	float delta = ((b*b)- (4*a*c));
+
 	if (delta < 0)
 	{
 		printf("Delta jest ujemna.\n");
 	}
-	if (delta > 0)
+	else if (delta > 0)
 	{
 		float x1 = (-1*b - sqrt(delta))/(2*a);
 		float x2 = (-1*b + sqrt(delta))/(2*a);
-		printf("Miejsca zeroswe funkcji x1: %f , x2: %f\n", x1, x2);
+		printf("Miejsca zerowe funkcji x1: %f , x2: %f\n", x1, x2);
 	}
+	else
+	{
+		float x = (-1*b)/(2*a);
+		printf("Miejsce zerowe funkcji x: %f\n", x);
+	}
+	
 }
